f17470be896d0e743142d2af344b49619905e6ad
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-09-26 14:16:12 -0700

    int

--- file1_printf.c
+++ file1_printf.c
@@ -73,3 +73,45 @@ int _printf(const char *format, ...)
         va_end(f_store);
         return (val_tally);
 }
+/**
+ * print_int - a function that prints int
+ * @types: lists of arguments
+ * @buffer: buffer array to handle print_int
+ * @flags:  calculates active flags in the function
+ * @width: gets the  width
+ * @precision: precision specification
+ * @size: size specifier
+ * Return: 0
+ */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+	{
+		buffer[i--] = '0';
+	}
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+	i++;
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
